c51d1733e90d2b8059864de8f1706f22ebe3b506
agungsptr <agung.e.sptr@gmail.com>
2024-07-24 15:56:53 +0800

    dev: update favicon and web title

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,11 +1,11 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Blog',
+  title: 'Agungsptr Blog',
   author: 'Agung Saputra',
   headerTitle: 'Agungsptr Blog',
   description: 'A blog about programming and my experience through it',
   language: 'en-us',
-  theme: 'system', // system, dark or light
+  theme: 'light', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
   siteRepo: '',
   siteLogo: '/static/images/logo.png',
